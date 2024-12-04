de1b3aa453ec2bb1c3a98a21b5e35d24466735f6
KEJ <starree0466@gmail.com>
2024-12-05 07:14:45 +0900

    커밋4 : 사용자 입력 반복 기능 추가
    숫자가 맞을 때까지 반복하도록 'do-while'문을 이용하여 루프를 추가

--- GeussNumGame/GeussNumGame/main.c
+++ GeussNumGame/GeussNumGame/main.c
@@ -6,19 +6,33 @@ int main() {
     srand(time(NULL));
     int target = rand() % 100 + 1;
     int guess;
+    int attempts = 0;
 
     printf("숫자가 생성되었습니다. 1부터 100까지의 숫자를 맞춰보세요 !\n");
-    printf("숫자를 입력하세요: ");
+    printf("숫자를 입력하세요 : ");
     scanf_s("%d", &guess);
+    attempts++;
 
-    if (guess == target) 
+    do 
     {
-        printf("축하합니다! 숫자를 맞추셨습니다.\n");
-    }
-    else 
-    {
-        printf("틀렸습니다! 다시 시도하세요.\n");
-    }
+        printf("숫자를 입력하세요 : ");
+        scanf_s("%d", &guess);
+        attempts++;
+
+        if (guess > target) 
+        {
+            printf("더 작은 숫자입니다.\n");
+        }
+        else if (guess < target) 
+        {
+            printf("더 큰 숫자입니다.\n");
+        }
+        else 
+        {
+            printf("축하합니다 ! %d번 만에 숫자를 맞추셨습니다.\n", attempts);
+        }
+    } while (guess != target);
+
 
     return 0;
 }
