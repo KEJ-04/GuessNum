6384c974646875446c0f9cf0fec854dde927d764
KEJ <starree0466@gmail.com>
2024-12-05 07:08:53 +0900

    커밋3 : 사용자 입력 기능 추가
    사용자로부터 숫자를 입력받고, 생성된 랜덤 숫자와 비교하는 로직 추가

--- GeussNumGame/GeussNumGame/main.c
+++ GeussNumGame/GeussNumGame/main.c
@@ -5,6 +5,20 @@
 int main() {
     srand(time(NULL));
     int target = rand() % 100 + 1;
-    printf("숫자가 생성되었습니다. 맞춰보세요!\n");
+    int guess;
+
+    printf("숫자가 생성되었습니다. 1부터 100까지의 숫자를 맞춰보세요 !\n");
+    printf("숫자를 입력하세요: ");
+    scanf_s("%d", &guess);
+
+    if (guess == target) 
+    {
+        printf("축하합니다! 숫자를 맞추셨습니다.\n");
+    }
+    else 
+    {
+        printf("틀렸습니다! 다시 시도하세요.\n");
+    }
+
     return 0;
 }
